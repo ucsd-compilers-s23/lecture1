6cc6461e0fb96b00045070cae1f6674852dca092
Joe Politz <joe@cs.brown.edu>
2023-04-17 08:54:23 -0700

    week3 monday

--- runtime/start.rs
+++ runtime/start.rs
@@ -7,9 +7,18 @@ extern "C" {
     fn our_code_starts_here() -> i64;
 }
 
+fn print(val : i64) {
+  if val == 3 { println!("true"); }
+  if val == 1 { println!("false"); }
+  if val % 2 == 0 { println!("{}", val >> 1); }
+  else {
+    println!("Unknown value: {}", val);
+  }
+}
+
 fn main() {
     let i : i64 = unsafe { our_code_starts_here()  };
-    println!("{i}");
+    println!("{}", i);
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,28 @@ use im::HashMap;
 
 enum Expr {
     Num(i32),
+    True, False,
     Add1(Box<Expr>),
-
     Plus(Box<Expr>, Box<Expr>),
-
     Let(String, Box<Expr>, Box<Expr>),
-
     Id(String),
+    Eq(Box<Expr>, Box<Expr>),
+    If(Box<Expr>, Box<Expr>, Box<Expr>)
 }
 
 fn parse_expr(s: &Sexp) -> Expr {
     match s {
         Sexp::Atom(I(n)) => Expr::Num(i32::try_from(*n).unwrap()),
+        Sexp::Atom(S(name)) if name == "true" => Expr::True,
+        Sexp::Atom(S(name)) if name == "false" => Expr::False,
         Sexp::Atom(S(name)) => Expr::Id(name.to_string()),
         Sexp::List(vec) => match &vec[..] {
             [Sexp::Atom(S(op)), e] if op == "add1" => Expr::Add1(Box::new(parse_expr(e))),
             [Sexp::Atom(S(op)), e1, e2] if op == "+" => {
                 Expr::Plus(Box::new(parse_expr(e1)), Box::new(parse_expr(e2)))
+            }
+            [Sexp::Atom(S(op)), e1, e2] if op == "=" => {
+                Expr::Eq(Box::new(parse_expr(e1)), Box::new(parse_expr(e2)))
             }
             [Sexp::Atom(S(keyword)), Sexp::List(vec), body] if keyword == "let" => match &vec[..] {
                 [Sexp::Atom(S(name)), val] => Expr::Let(
@@ -34,23 +39,54 @@ fn parse_expr(s: &Sexp) -> Expr {
                 ),
                 _ => panic!("parse error"),
             },
-            _ => panic!("parse error"),
+            [Sexp::Atom(S(keyword)), cond, thn, els] if keyword == "if" => {
+                Expr::If(Box::new(parse_expr(cond)), Box::new(parse_expr(thn)), Box::new(parse_expr(els)))
+            },
+
+            _ => panic!("parse error: {}", s),
         },
         _ => panic!("parse error"),
     }
 }
 
-fn compile_expr(e: &Expr, si: i32, env: &HashMap<String, i32>) -> String {
+fn new_label(l : &mut i32, s : &str) -> String {
+  let current = *l;
+  *l += 1;
+  format!("{s}_{current}")
+}
+
+fn compile_expr(e: &Expr, si: i32, env: &HashMap<String, i32>, l: &mut i32) -> String {
     match e {
-        Expr::Num(n) => format!("mov rax, {}", *n),
+        Expr::Num(n) => format!("mov rax, {}", *n << 1),
+        Expr::True => format!("mov rax, {}", 3),
+        Expr::False => format!("mov rax, {}", 1),
         Expr::Id(s) => {
             let offset = env.get(s).unwrap() * 8;
             format!("mov rax, [rsp - {offset}]")
         }
-        Expr::Add1(subexpr) => compile_expr(subexpr, si, env) + "\nadd rax, 1",
+        Expr::Add1(subexpr) => compile_expr(subexpr, si, env, l) + "\nadd rax, 1",
+        Expr::Eq(e1, e2) => {
+            format!("mov rax, 1")
+        },
+        Expr::If(cond, thn, els) => {
+           let end_label = new_label(l, "ifend"); 
+           let else_label = new_label(l, "ifelse"); 
+           let cond_instrs = compile_expr(cond, si, env, l);
+           let thn_instrs = compile_expr(thn, si, env, l);
+           let els_instrs = compile_expr(els, si, env, l);
+           format!("
+              {cond_instrs}
+              cmp rax, 1
+              je {else_label}
+                {thn_instrs}
+              {else_label}:
+                {els_instrs}
+              {end_label}:
+           ")
+        }
         Expr::Plus(e1, e2) => {
-            let e1_instrs = compile_expr(e1, si, env);
-            let e2_instrs = compile_expr(e2, si + 1, env);
+            let e1_instrs = compile_expr(e1, si, env, l);
+            let e2_instrs = compile_expr(e2, si + 1, env, l);
             let stack_offset = si * 8;
             format!(
                 "
@@ -62,8 +98,8 @@ fn compile_expr(e: &Expr, si: i32, env: &HashMap<String, i32>) -> String {
             )
         }
         Expr::Let(name, val, body) => {
-            let val_is = compile_expr(val, si, env);
-            let body_is = compile_expr(body, si + 1, &env.update(name.to_string(), si));
+            let val_is = compile_expr(val, si, env, l);
+            let body_is = compile_expr(body, si + 1, &env.update(name.to_string(), si), l);
             let offset = si * 8;
             format!(
                 "
@@ -87,7 +123,8 @@ fn main() -> std::io::Result<()> {
     in_file.read_to_string(&mut in_contents)?;
 
     let expr = parse_expr(&parse(&in_contents).unwrap());
-    let result = compile_expr(&expr, 2, &HashMap::new());
+    let mut labels = 0;
+    let result = compile_expr(&expr, 2, &HashMap::new(), &mut labels);
     let asm_program = format!(
         "
 section .text
